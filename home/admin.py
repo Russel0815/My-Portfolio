a933ca22715ef4af1468aeb2eea119be1470ae16
Russel0815 <lacanlalerusselivan@gmail.com>
2026-07-09 18:45:09 +0800

    Clean up project structure, remove compiled cache files, and finalize Quiz 2 backend views

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import PersonalInformation, Project
 
-# Register your models here.
+admin.site.register(PersonalInformation)
+admin.site.register(Project)
